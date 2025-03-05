72d6b6fac86adb72f36462279ea526b05bf0ac9a
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-03-05 11:07:31 +0100

    Set up challenge 1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-// Example TypeScript code
-const greeting = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+/**
+ * Write a function that prints numbers from 1 to n
+ * if a number is divisible by 3, print "Fizz"
+ * if a number is divisible by 5, print "Buzz"
+ * if a number is divisible by both 3 and 5, print "FizzBuzz"
+ * Otherwise, print the number
+ */
 
-console.log(greeting('Worldly Persons')); 
+
+
