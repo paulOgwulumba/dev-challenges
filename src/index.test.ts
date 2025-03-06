629224a05dc1d76caa658b6aec0503c70dbc99fc
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-03-06 13:25:14 +0100

    Set up tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,31 @@
-import { findMissingNumbers } from './index';
+import { twoSum } from './index';
 
-describe('findMissingNumbers', () => {
-  test('Detects missing numbers', () => {
-    expect(findMissingNumbers([3, 0, 1], 3)).toEqual([2]);
-    expect(findMissingNumbers([9, 6, 4, 3, 5, 7, 0, 1], 10)).toEqual([2, 8, 10]);
-    expect(findMissingNumbers([1, 2, 3, 4, 5], 5)).toEqual([0]);
+function checkResult(result: number[], input: number[], target: number) {
+  if (result.length !== 2) {
+    return false;
+  }
+  
+  if (result[0] === result[1]) {
+    return false;
+  }
+
+  if (input[result[0]] + input[result[1]] !== target) {
+    return false;
+  }
+
+  return true;
+}
+
+describe('twoSum', () => {
+  test('Detects two numbers that add up to the target', () => {
+    expect(checkResult(twoSum([2, 7, 11, 15], 9), [2, 7, 11, 15], 9)).toBe(true);
+    expect(checkResult(twoSum([3, 2, 4], 6), [3, 2, 4], 6)).toBe(true);
+    expect(checkResult(twoSum([1, 2, 3, 4, 5, 6], 6), [1, 2, 3, 4, 5, 6], 6)).toBe(true);
+    expect(checkResult(twoSum([1, 2, 3, 4, 5, 6, 7], 7), [1, 2, 3, 4, 5, 6, 7], 7)).toBe(true);
   });
 
-  test('Returns empty array for no missing numbers', () => {
-    expect(findMissingNumbers([0, 1, 2, 3, 4, 5, 6], 6)).toEqual([]);
-    expect(findMissingNumbers([0, 1, 2, 3, 4, 5, 6, 7], 7)).toEqual([]);
+  test('Returns empty array for no two numbers that add up to the target', () => {
+    expect(twoSum([1,2,3], 6)).toEqual([]);
+    expect(twoSum([1,2,3,4,5], 10)).toEqual([]);
   });
 }); 
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
 /**
   * Given an array of numbers and a target sum, return the indices of the two numbers that add up to the target.
+  * If no two numbers add up to the target, return an empty array.
   * Example:
   * twoSum([2, 7, 11, 15], 9) // [0, 1] 
   * twoSum([3, 2, 4], 6) // [1, 2]
   */
 
 export function twoSum(arr: number[], target: number): number[] {
-  const map = new Map();
-
-  for (let i = 0; i < arr.length; i++) {
-    const complement = target - arr[i];
-
-    if (map.has(complement)) {
-      return [map.get(complement), i];
-    }
-
-    map.set(arr[i], i);
-  }
-
-  return [];
+  
 }
 
