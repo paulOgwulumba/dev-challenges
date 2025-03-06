8d5d4b36eed3310c6c3ec6490652535a0d4f5891
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-03-06 13:14:47 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /**
  * Given an array containing n distinct numbers from 0 to n, return a sorted array of the missing numbers.
  * Example:
- * findMissingNumber([0, 1, 3], 3) // [2]
- * findMissingNumber([9, 6, 4, 3, 5, 7, 0, 1], 10) // [2, 8]
+ * findMissingNumbers([0, 1, 3], 3) // [2]
+ * findMissingNumbers([9, 6, 4, 3, 5, 7, 0, 1], 10) // [2, 8]
  */
 
 export function findMissingNumbers(arr: number[], n: number): number[] {
