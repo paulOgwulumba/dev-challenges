a98e154266c5735364e7b5053235f56f325c4f6e
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-03-06 13:13:35 +0100

    Set up tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,14 @@
-import { firstUniqueChar } from './index';
+import { findMissingNumbers } from './index';
 
-describe('firstUniqueChar', () => {
-  test('Detects first unique character', () => {
-    expect(firstUniqueChar('swiss')).toBe('w');
-    expect(firstUniqueChar('hello')).toBe('h');
-    expect(firstUniqueChar('badalandabad')).toBe('l');
+describe('findMissingNumbers', () => {
+  test('Detects missing numbers', () => {
+    expect(findMissingNumbers([3, 0, 1], 3)).toEqual([2]);
+    expect(findMissingNumbers([9, 6, 4, 3, 5, 7, 0, 1], 10)).toEqual([2, 8, 10]);
+    expect(findMissingNumbers([1, 2, 3, 4, 5], 5)).toEqual([0]);
   });
 
-  test('Returns null for no unique character', () => {
-    expect(firstUniqueChar('')).toBeNull();
-    expect(firstUniqueChar('aabbcc')).toBeNull();
-    expect(firstUniqueChar('fababbcdcddeeff')).toBeNull();
+  test('Returns empty array for no missing numbers', () => {
+    expect(findMissingNumbers([0, 1, 2, 3, 4, 5, 6], 6)).toEqual([]);
+    expect(findMissingNumbers([0, 1, 2, 3, 4, 5, 6, 7], 7)).toEqual([]);
   });
 }); 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /**
- * Given an array containing n distinct numbers from 0 to n, find the missing number.
+ * Given an array containing n distinct numbers from 0 to n, return a sorted array of the missing numbers.
  * Example:
- * findMissingNumber([3, 0, 1]) // 2
+ * findMissingNumber([0, 1, 3], 3) // [2]
+ * findMissingNumber([9, 6, 4, 3, 5, 7, 0, 1], 10) // [2, 8]
  */
 
-export function firstUniqueChar(str: string): string | null {
+export function findMissingNumbers(arr: number[], n: number): number[] {
 
 }
 
