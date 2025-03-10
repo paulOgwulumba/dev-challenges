f49431933d86581082b01ba9d1120aaf1f78a35f
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-03-10 15:00:28 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * Write a function that checks if a given string is a palindrome (reads the same forward and backward).
+ * Write a function that checks if a given word is a palindrome (reads the same forward and backward).
  * Example:
  * isPalindrome("racecar") // true
  * isPalindrome("hello") // false
